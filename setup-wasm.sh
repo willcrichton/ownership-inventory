067a8d2752d5f5b44ef8a78c026f52cd271d75ec
Will Crichton <wcrichto@cs.stanford.edu>
2022-10-06 17:55:19 -0400

    Fix CI

--- setup-wasm.sh
+++ setup-wasm.sh
@@ -1,6 +1,7 @@
-git clone -b 466bf192c3cbaa943fd5bb7fe502978111957e41 https://github.com/rust-analyzer/rust-analyzer-wasm
+git clone https://github.com/rust-analyzer/rust-analyzer-wasm
 rustup toolchain install nightly-2021-11-02 --profile minimal -c rust-src
 pushd rust-analyzer-wasm
+git checkout 466bf192c3cbaa943fd5bb7fe502978111957e41
 cd rust-pack
 cargo run
 cd ../ra-wasm
