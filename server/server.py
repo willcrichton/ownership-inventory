1fc01359ead5f26b5e81ac6ec26a7460764ee463
Will Crichton <wcrichto@cs.stanford.edu>
2022-10-06 21:40:06 +0000

    Sanitize paths on server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from pathlib import Path
+from pathvalidate import sanitize_filename
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,6 @@ CORS(app)
 def upload_file():
     recording = next(iter(request.files.keys()))
     f = request.files[recording]
-    f.save(Path('recordings') / recording)
+    f.save(Path('recordings') / sanitize_filename(recording))
     return 'file uploaded successfully'
 		
