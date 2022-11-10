56edea2a831c5f29dd35aae5db9a7e490fba3be6
willcrichton <willcrichton@users.noreply.github.com>
2022-11-10 02:56:07 +0000

    deploy: 7efcc43db6ef70040c6df3091146806c61a0e510

--- ownership-inventory/intro.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export declare let Intro: ({ next }: {
-    next: (email: string) => void;
-}) => JSX.Element;
--- ownership-inventory/tutorial.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { RustAnalyzer } from "@wcrichto/rust-editor";
-export declare let Tutorial: ({ next, ra, }: {
-    next: () => void;
-    ra?: RustAnalyzer | undefined;
-}) => JSX.Element;
