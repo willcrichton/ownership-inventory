e6daaae6c20206c14717150ae19ee71279fb4020
willcrichton <willcrichton@users.noreply.github.com>
2022-11-11 20:01:47 +0000

    deploy: d784fa5831594a056a96b980e25af699bd00148a

--- ownership-inventory/assets/ra-worker.665d4bb5.js
+++
@@ -1,1 +0,0 @@
-import i,{initThreadPool as n,WorldState as c}from"./wasm_demo.53dabd9c.js";import"./workerHelpers.8c811a52.js";const d=async()=>{await i(),await n(navigator.hardwareConcurrency);const t=new c;onmessage=a=>{const{which:r,args:s,id:e}=a.data,o=t[r](...s);postMessage({id:e,result:o})}};d().then(()=>{postMessage({id:"ra-worker-ready"})});
