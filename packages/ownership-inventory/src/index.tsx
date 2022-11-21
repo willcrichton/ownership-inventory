62c11e177511d99c699a73553fe8755d0dbc4d9a
Will Crichton <wcrichto@cs.stanford.edu>
2022-11-20 21:29:51 -0600

    Update problem set

--- packages/ownership-inventory/src/index.tsx
+++ packages/ownership-inventory/src/index.tsx
@@ -112,11 +112,9 @@ interface ExperimentData {
 
 let App = () => {
   // let problems = useMemo(() => _.sampleSize(PROBLEMS, 3), []);
-  let problems = _.shuffle([
-    "make_separator",
-    "get_or_default",
-    "remove_zeros",
-  ]).map(name => _.find(PROBLEMS, { name })!);
+  let problems = _.shuffle(["find_nth", "apply_curve", "add_displayable"]).map(
+    name => _.find(PROBLEMS, { name })!
+  );
   let id = useMemo(() => uuid.v4(), []);
   let start = useMemo(() => new Date().getTime(), []);
   let answers = useMemo<TaggedAnswer[]>(() => [], []);
